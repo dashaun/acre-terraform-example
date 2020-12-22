0831b214049e9cdebb5886ccfddd30423437dc6e
DaShaun <dashaun@redislabs.com>
2020-12-22 17:11:30 -0600

    First pass

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # acre-terraform-example
 Deploy Azure Cache Enterprise Tiers using Terraform and ARM template
+
+## Getting started
+
+- Clone the repository
+- Copy terraform.tfvars.example to terraform.tfvars
+- Update terraform.tfvars with your values
+- terraform init
+- terraform plan
+- terraform apply
---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "acre_name" {
+  description = "The name of the deployment"
+  type = string
+}
+
+variable "acre_resource_group" {
+  description = "The name of the resource group being deployed to"
+  type = string
+}
+
+variable "acre_deployment_mode" {
+  type = string
+  default = "Complete"
+}
+
+variable "acre_template_path" {
+  description = "Path to ARM template being sourced"
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "cloud_name" {
+  description = "The Azure cloud environment to use. Available values at https://www.terraform.io/docs/providers/azurerm/#environment"
+  default     = "public"
+  type        = string
+}
+
+variable "tags" {
+  description = "Key/value tags to assign to all resources."
+  default     = {}
+  type        = map(string)
+}
